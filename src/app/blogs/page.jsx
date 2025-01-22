5273ef8b316c876ed6e5b2345c9602bdd263903c
Muhammad Ranju <mdranju23@gmail.com>
2025-01-23 00:17:20 +0600

    feat: add blog details page and enhance blogs listing with dynamic data fetching

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,5 +1,41 @@
-const page = () => {
-  return <div>Blogs</div>;
-};
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-export default page;
+const Blogs = async () => {
+  const res = await fetch(" https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+
+  return (
+    <div className="container mx-auto px-4 py-16 sm:px-6 lg:py-24 lg:px-8">
+      <div className="flex flex-col items-center mb-10 justify-center">
+        <h1 className="text-2xl font-bold">All Blogs</h1>
+        <p>You can find all the blogs here.</p>
+      </div>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-6 4xl:grid-cols-7 5xl:grid-cols-8 6xl:grid-cols-9 auto-rows-fr">
+        {posts.map((post) => (
+          <div
+            key={post.id}
+            className="flex border p-5 rounded-lg hover:shadow transition-shadow ease-in-out duration-300  capitalize space-y-3 flex-col items-center justify-center"
+          >
+            <Link href={`/blogs/${post.id}`}>
+              <h1 className="text-xl font-bold">
+                {post.title.length > 20
+                  ? post.title.slice(0, 25) + "..."
+                  : post.title}
+              </h1>
+            </Link>
+            <p className="text-sm text-slate-700">
+              {post.body.length > 100
+                ? post.body.slice(0, 150) + "..."
+                : post.body}
+            </p>
+            <Link href={`/blogs/${post.id}`}>
+              <Button>Read More</Button>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Blogs;
--- src/components/Navbar/NavbarComponent.jsx
+++ src/components/Navbar/NavbarComponent.jsx
@@ -16,23 +16,8 @@ export const NavbarComponent = () => {
             title="Company"
             className="inline-flex items-center"
           >
-            <svg
-              className="w-8 text-deep-purple-accent-400"
-              viewBox="0 0 24 24"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeMiterlimit="10"
-              stroke="currentColor"
-              fill="none"
-            >
-              <rect x="3" y="1" width="7" height="12" />
-              <rect x="3" y="17" width="7" height="6" />
-              <rect x="14" y="1" width="7" height="6" />
-              <rect x="14" y="11" width="7" height="12" />
-            </svg>
-            <span className="ml-2 text-xl font-bold tracking-wide text-black uppercase">
-              Blogs
+            <span className="ml-2 text-3xl font-bold tracking-wide text-black uppercase">
+              Blo<span className="text-purple-800">gs</span>
             </span>
           </Link>
           <ul className="flex items-center hidden space-x-8 lg:flex">
@@ -112,23 +97,8 @@ export const NavbarComponent = () => {
                         title="Company"
                         className="inline-flex items-center"
                       >
-                        <svg
-                          className="w-8 text-deep-purple-accent-400"
-                          viewBox="0 0 24 24"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeMiterlimit="10"
-                          stroke="currentColor"
-                          fill="none"
-                        >
-                          <rect x="3" y="1" width="7" height="12" />
-                          <rect x="3" y="17" width="7" height="6" />
-                          <rect x="14" y="1" width="7" height="6" />
-                          <rect x="14" y="11" width="7" height="12" />
-                        </svg>
-                        <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                          Blogs
+                        <span className="ml-2 text-2xl font-bold tracking-wide text-black uppercase">
+                          Blo<span className="text-purple-800">gs</span>
                         </span>
                       </Link>
                     </div>
