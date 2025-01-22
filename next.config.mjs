bbf30c53b6e17fcf78adeff7fc7f483ca455c49f
Muhammad Ranju <mdranju23@gmail.com>
2025-01-23 03:16:24 +0600

    feat: add environment variables for Kinde authentication URLs in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    KINDE_SITE_URL:
+      process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGOUT_REDIRECT_URL:
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ??
+      `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGIN_REDIRECT_URL:
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ??
+      `https://${process.env.VERCEL_URL}/profile`,
+  },
   images: {
     domains: ["picsum.photos", "gravatar.com"],
     unoptimized: true,
