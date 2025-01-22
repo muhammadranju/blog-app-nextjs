2ad21578c0e8b482cc19ca436a53319f00d4002c
Muhammad Ranju <mdranju23@gmail.com>
2025-01-23 02:32:19 +0600

    feat: enhance Kinde authentication GET handler with secure cookie options

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -1,2 +1,7 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+export const GET = handleAuth({
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+  },
+});
